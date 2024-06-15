15169454209392a8d0add0add0433d76a0586abe
vinilope <vinicius.zirondi@sptech.school>
2024-06-15 14:14:19 -0300

    add repo inicial

---
+++ src/main/java/com/mednow/mednowapi/MednowapiApplication.java
@@ -0,0 +1,13 @@
+package com.mednow.mednowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MednowapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MednowapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mednow/mednowapi/MednowapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mednow.mednowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MednowapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
