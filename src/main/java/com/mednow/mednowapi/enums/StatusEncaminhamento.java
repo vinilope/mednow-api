319daaebb60713e04ea8b092916c6bb34083d722
vinilope <vinicius.zirondi@sptech.school>
2024-06-16 20:53:57 -0300

    add construtores enum

--- src/main/java/com/mednow/mednowapi/enums/StatusEncaminhamento.java
+++ src/main/java/com/mednow/mednowapi/enums/StatusEncaminhamento.java
@@ -1,6 +1,16 @@
 package com.mednow.mednowapi.enums;
 
+import lombok.Builder;
+
 public enum StatusEncaminhamento {
-    PENDENTE,
-    FINALIZADO
+    PENDENTE(1, "pending"),
+    FINALIZADO(2, "finished");
+
+    private Integer statusCod;
+    private String statusValue;
+
+    StatusEncaminhamento(Integer statusCod, String statusValue) {
+        this.statusCod = statusCod;
+        this.statusValue = statusValue;
+    }
 }
--- src/main/java/com/mednow/mednowapi/enums/TipoUsuario.java
+++ src/main/java/com/mednow/mednowapi/enums/TipoUsuario.java
@@ -1,7 +1,14 @@
 package com.mednow.mednowapi.enums;
 
 public enum TipoUsuario {
-    ADMIN,
-    MEDICO,
-    PACIENTE
+    ADMIN(0, "adm"),
+    MEDICO(1, "med");
+
+    private Integer userCod;
+    private String userValue;
+
+    TipoUsuario(Integer userCod, String userValue) {
+        this.userCod = userCod;
+        this.userValue = userValue;
+    }
 }
