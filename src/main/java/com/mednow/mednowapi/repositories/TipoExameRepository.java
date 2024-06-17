d1564be2db231940118804f98c48bca14d428f85
vinilope <vinicius.zirondi@sptech.school>
2024-06-16 23:51:30 -0300

    fix id TipoExame

--- src/main/java/com/mednow/mednowapi/repositories/TipoExameRepository.java
+++ src/main/java/com/mednow/mednowapi/repositories/TipoExameRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface TipoExameRepository extends JpaRepository<TipoExame, UUID> {
+public interface TipoExameRepository extends JpaRepository<TipoExame, Integer> {
 }
