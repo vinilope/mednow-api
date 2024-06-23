f1dc9c6f2e5d8c7680401ba6128943c582bec826
vinilope <vinicius.zirondi@sptech.school>
2024-06-22 21:22:21 -0300

    update retorno especialidade para string

--- src/main/java/com/mednow/mednowapi/dtos/responses/MedicoResponse.java
+++ src/main/java/com/mednow/mednowapi/dtos/responses/MedicoResponse.java
@@ -1,5 +1,6 @@
 package com.mednow.mednowapi.dtos.responses;
 
+import com.mednow.mednowapi.enums.EspecialidadeMedico;
 import com.mednow.mednowapi.models.Medico;
 import lombok.Getter;
 
@@ -9,7 +10,7 @@ public class MedicoResponse {
    private final String crm;
    private final String telefone;
    private final String email;
-   private final Integer especialidade;
+   private final String especialidade;
    private final UsuarioResponse usuario;
 
     public MedicoResponse(Medico medico) {
@@ -17,7 +18,7 @@ public class MedicoResponse {
         this.crm = medico.getCrm();
         this.telefone = medico.getTelefone();
         this.email = medico.getEmail();
-        this.especialidade = medico.getEspecialidade();
+        this.especialidade = EspecialidadeMedico.valueOf(medico.getEspecialidade()).getEspecialidade();
         this.usuario = new UsuarioResponse(medico.getUsuario());
     }
 }
